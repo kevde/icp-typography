cbbc79083c942630beb469da55825aaf906ba330
Paul Jaceldo <paul.jaceldo@gmail.com>
2022-01-09 00:34:00 +0800

    Added changes

--- stories/Typography.tsx
+++ stories/Typography.tsx
@@ -19,6 +19,35 @@ interface TypographyProps {
   primary?: boolean;
 }
 
+const THICKNESS: any = {
+  HEADLINE: 700,
+  SEMI_BOLD: 600,
+  MEDIUM: 500,
+  REGULAR: 400
+}
+
+const VARIANT_DETAILS: any = {
+  h1: { fontWeight: THICKNESS.HEADLINE, fontSize: '48pt' },
+  h2: { fontWeight: THICKNESS.SEMI_BOLD, fontSize: '40pt' },
+  h3: { fontWeight: THICKNESS.SEMI_BOLD, fontSize: '32pt' },
+  h4: { fontWeight: THICKNESS.SEMI_BOLD, fontSize: '24pt' },
+  s1: { fontWeight: THICKNESS.SEMI_BOLD, fontSize: '18pt' },
+  s2: { fontWeight: THICKNESS.MEDIUM, fontSize: '18pt' },
+  s3: { fontWeight: THICKNESS.SEMI_BOLD, fontSize: '16pt' },
+  s4: { fontWeight: THICKNESS.MEDIUM, fontSize: '16pt' },
+  s5: { fontWeight: THICKNESS.SEMI_BOLD, fontSize: '14pt' },
+  p1: { fontWeight: THICKNESS.MEDIUM, fontSize: '14pt' },
+  p2: { fontWeight: THICKNESS.REGULAR, fontSize: '14pt' },
+  p3: { fontWeight: THICKNESS.MEDIUM, fontSize: '12pt' },
+  p4: { fontWeight: THICKNESS.REGULAR, fontSize: '12pt' },
+  c1: { fontWeight: THICKNESS.MEDIUM, fontSize: '12pt' },
+  c2: { fontWeight: THICKNESS.REGULAR, fontSize: '12pt' },
+  c3: { fontWeight: THICKNESS.MEDIUM, fontSize: '10pt' },
+  c4: { fontWeight: THICKNESS.REGULAR, fontSize: '10pt' },
+  c5: { fontWeight: THICKNESS.MEDIUM, fontSize: '8pt' },
+  c6: { fontWeight: THICKNESS.REGULAR, fontSize: '8pt' },
+}
+
 export const Typography = ({
   children = 'Default Text',
   variant = 'h1',
@@ -26,11 +55,8 @@ export const Typography = ({
   primary = false,
 }: TypographyProps) => (
   <ThemeProvider theme={darkTheme}>
-    <div>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat" />
-      <MuiTypography variant={variant}>
+      <MuiTypography variant={variant} style={VARIANT_DETAILS[variant]}>
         {children}
       </MuiTypography>
-    </div>
   </ThemeProvider>
 );
